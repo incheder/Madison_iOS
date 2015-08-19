39a5621b5ef7b91247fd0c3633c2ca4c04d87a7d
eder <edpefal@gmail.com>
2015-08-19 08:45:29 -0500

    Collection View Test

---
+++ Madison/CategorieViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CategorieViewCell.swift
+//  Madison
+//
+//  Created by Eder Perez Falcon on 8/19/15.
+//  Copyright (c) 2015 wezen. All rights reserved.
+//
+
+import UIKit
+
+class CategorieViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
